9bb378ba3a42fafabf5c82416f020a582ac87be1
E526729 <ahmed.zekri.a@gmail.com>
2022-10-30 00:13:34 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = '8292daa9-918d-431f-a95e-e10c057f8e7d';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
